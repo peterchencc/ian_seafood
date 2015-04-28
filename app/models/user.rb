d21d76f700278be955f583cfaa913ad90cfec62f
Wen-Tien Chang <ihower@gmail.com>
2015-04-28 23:10:21 +0800

    Install devise + omniauth-facebook and user can login via Facebook

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,41 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable,
+         :omniauthable, :omniauth_providers => [:facebook]
 
   before_save :ensure_authentication_token
+
+  def self.from_omniauth(auth)
+    user = where( fb_uid: auth.uid ).first
+
+    unless user
+      user = self.new
+      user.fb_uid = auth.uid
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0,20]
+    end
+
+    if auth.credentials
+      user.fb_access_token = auth.credentials.token
+      user.fb_expires_at = Time.at(auth.credentials.expires_at)
+    end
+
+    user.save
+    user
+  end
+
+  # 從 Facebook 認證回來時，如果該 fb_uid 不存在
+  # self.from_omniauth 會試圖直接新增 User
+  # 如果新增失敗(例如有其他 validation 不過)，devise 就會導去註冊頁面
+  # 而 self.new_with_session 這個方法提供註冊頁面的預設值
+  def self.new_with_session(params, session)
+    super.tap do |user|
+      if data = session["devise.facebook_data"] && session["devise.facebook_data"]["extra"]["raw_info"]
+        user.email = data["email"] if user.email.blank?
+      end
+    end
+  end
 
   def generate_authentication_token
     token = nil
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -234,6 +234,10 @@ Devise.setup do |config|
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
 
+  fb_config = YAML.load(File.read("#{Rails.root}/config/facebook.yml"))[Rails.env]
+
+  config.omniauth :facebook, fb_config["app_id"], fb_config["secret"]
+
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
   # change the failure app, you can configure them inside the config.warden block.
