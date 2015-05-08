27debcd8ce7a874ced276206bdf476eb57a647bf
vneverz <vneverz@gmail.com>
2015-05-08 19:34:13 +0800

    Fix migration

---
+++ app/controllers/api_v1/orders_controller.rb
@@ -0,0 +1,24 @@
+class ApiV1::OrdersController < ApiController
+
+  def create
+    @order = Order.new( name: params[:name],
+                        phone: params[:phone],
+                        address: params[:address],
+                        email: params[:email] )
+
+    Array(params[:line_items]).each do |line_item|
+      @order.line_items.build( :product_id => line_item[:product_id],
+                               :packet_id => line_item[:packet_id],
+                               :qty => line_item[:qty] )
+    end
+
+    @order.user = current_user
+
+    if @order.save
+      render :json => { :message => "OK", :order_id => @order.id }
+    else
+      render :json => { :errors => @order.errors.full_messages }, :status => 400
+    end
+  end
+
+end
--- db/migrate/20150508071436_removw_lineitem_unitprice.rb
+++
@@ -1,5 +0,0 @@
-class RemovwLineitemUnitprice < ActiveRecord::Migration
-  def change
-    remove_column :line_items, :unit_price
-  end
-end
