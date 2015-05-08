b9d4b08aa10afe9e5b26ec90e3c50028d3695cf6
vneverz <vneverz@gmail.com>
2015-05-08 20:33:58 +0800

    can add product and style pagination

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -11,6 +11,24 @@ class ProductsController < ApplicationController
     @product = Product.find(params[:id])
   end
 
+  def new
+    @product = Product.new
+  end
+
+  def create
+  @product = Product.new( product_params )
+
+    if @product.save
+
+      flash[:notice] = I18n.t("create_successful")
+
+      redirect_to products_path
+    else
+      load_products
+      render :action => :index
+    end
+  end
+
   def edit
     @product = Product.find(params[:id])
   end
